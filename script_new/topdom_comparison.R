abf60f67a3c6b83bb3b9ba51d82e198cd20232a5
ZexunWu <58058314+Tim-wuuu@users.noreply.github.com>
2023-01-11 19:01:01 -0800

    Update topdom_comparison.R

--- script_new/topdom_comparison.R
+++ script_new/topdom_comparison.R
@@ -1,37 +1,67 @@
 library(dplyr)
-library(ggplot2)
-library(plyr)
 library(fuzzyjoin)
 
-method_hic <- read.table("./input/HiC-50000-topdom.true.bed")
-method_micro <- read.table("./input/1_Billion-50000-topdom.ture.bed")
+topdom_comparison <- function(
+topdom_bed1,
+topdom_bed2,
+label_1 = "topdom_bed1",
+label_2 = "topdom_bed2",
+resolution
+){
+topdom_bed1$V1 <- gsub("chr","",topdom_bed1$V1)
+topdom_bed2$V1 <- gsub("chr","",topdom_bed2$V1)
 
-method_micro$V1 <- gsub("chr","",method_micro$V1)
+#put loop name
+topdom_bed1$loopname <- paste(label_1,rownames(topdom_bed1),sep="_")
+topdom_bed2$loopname <- paste(label_2,rownames(topdom_bed2),sep="_")
 
-method_hic$V1 <- gsub("chr","",method_hic$V1)
-
-method_hic <- method_hic %>% mutate( MicroC = 0,Capture_Micro_C = 1)
-method_micro <- method_micro %>% mutate (MicroC = 1,Capture_Micro_C = 0)
-
-
-method_hic$loopname <- paste("method_hic",rownames(method_hic),sep="_")
-method_micro$loopname <-paste("method_micro", rownames(method_micro),sep="_")
+#put comparison column
+topdom_bed1$comparison <- 0
+topdom_bed2$comparison <- 0
 
 
-method_hic <- method_hic %>% filter(V4 == "domain")
-method_micro <- method_micro %>% filter(V4 == "domain")
+#only perform analysis in "domain" within V4 column
+topdom_bed1 <- topdom_bed1 %>% filter(V4 == "domain")
+topdom_bed2 <- topdom_bed2 %>% filter(V4 == "domain")
 
 x <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,"X","Y")
 
-
-
-micro_2_hic <- data.frame()
+bed1_to_bed2 <- data.frame()
+bed2_to_bed1 <- data.frame()
 for (i in x){
-  chr_template <- method_micro %>% filter(V1 == i)
-  chr_reference <- method_hic %>% filter(V1 == i)
-  some_count <- difference_semi_join(chr_template,chr_reference, max_dist= 100000, by = c("V2","V3"))
-  micro_2_hic <-rbind(micro_2_hic,some_count)
+  chr_template <- topdom_bed1 %>% filter(V1 == i)
+  chr_reference <- topdom_bed2 %>% filter(V1 == i)
+  some_count <- difference_semi_join(chr_template,chr_reference, max_dist= resolution*2, by = c("V2","V3"))
+  some_count2 <- difference_semi_join(chr_reference,chr_template, max_dist= resolution*2, by = c("V2","V3"))
+  bed1_to_bed2 <- rbind(bed1_to_bed2,some_count)
+  bed2_to_bed1 <- rbind(bed2_to_bed1,some_count2)
 }
-method_micro$Capture_Micro_C[method_micro$loopname %in% micro_2_hic$loopname] <- 1
 
-nrow(hic_micro_2)
+#update comparison matrix
+bed1_to_bed2$comparison <- 1
+bed2_to_bed1$comparison <- 1
+topdom_bed1$comparison[topdom_bed1$loopname %in% bed1_to_bed2$loopname] <- 1
+topdom_bed2$comparison[topdom_bed2$loopname %in% bed2_to_bed1$loopname] <- 1
+
+#change column comparison to the labels
+colnames(topdom_bed1)[ncol(topdom_bed1)] <- label_2
+colnames(bed1_to_bed2)[ncol(bed1_to_bed2)] <- label_2
+colnames(topdom_bed2)[ncol(topdom_bed2)] <- label_1
+colnames(bed2_to_bed1)[ncol(bed2_to_bed1)] <- label_1
+
+#create list to save wanted outputs
+final <- list()
+final[[1]] <- bed1_to_bed2
+final[[2]] <- bed2_to_bed1
+final[[3]] <- topdom_bed1
+final[[4]] <- topdom_bed2
+
+#remove loopname column, change column comparison to the labels, put chr back in V1
+final <- lapply(final, function(x){
+	x <- subset(x, select = -loopname)
+	x$V1 <- paste0('chr', x$V1)
+	return(x)
+ 	}) #end lapply
+
+return(final)
+} #end function
